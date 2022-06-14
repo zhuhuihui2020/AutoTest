2f1f3e647115985f9624d0e9fd681cfe5450ad4c
xcy <534600451@qq.com>
2022-06-14 16:13:24 +0800

    code21

---
+++ test/leetcode/Code21.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+public class Code21 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1==null){
+            return list2;
+        }
+        if(list2==null){
+            return list1;
+        }
+        ListNode newList=new ListNode(0);
+        ListNode curr=newList;
+        while(list1!=null&&list2!=null){
+            if(list1.val< list2.val){
+                curr.next=list1;
+                list1=list1.next;
+            }else{
+                curr.next=list2;
+                list2=list2.next;
+            }
+            curr=curr.next;
+        }
+//        if(list1!=null){
+//            curr.next=list1;
+//        }
+//        if(list2!=null){
+//            curr.next=list2;
+//        }
+        curr.next=(list1==null)?list2:list1;
+        return newList.next;
+    }
+
+    public static int length(ListNode listNode){
+        int i=0;
+        while(listNode!=null){
+            i++;
+            listNode=listNode.next;
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        ListNode list1=new ListNode(1);
+        ListNode list2=new ListNode(2,list1);
+        ListNode list3=new ListNode(3,list2);
+        System.out.println(Code21.length(list3));
+    }
+}
+
+
