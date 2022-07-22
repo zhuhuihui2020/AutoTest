8f9fdee6d45d5df2eaed566c9b4e7bfdce516393
zhuhuihui <zhuhh2019@163.com>
2022-07-22 18:46:55 +0800

    commit code5

--- test/leetcode/Code14_2.java
+++ test/leetcode/Code14_2.java
@@ -1,6 +1,6 @@
 package leetcode;
-//最长公共前缀--纵向扫描
-public class Code14 {
+//最长公共前缀--纵向扫描,稍微改进一下
+public class Code14_2 {
     public static String longestCommonPrefix(String[] strs) {
         for (String str :strs) {
             if(str.equals("")){
@@ -16,16 +16,14 @@ public class Code14 {
                 minIndex=i+1;
             }
         }
-//        StringBuffer str=new StringBuffer();
-        //将StringBuffer换成StringBuilder，性能更高
         StringBuilder str=new StringBuilder();
         int j=0;
         //截取的长度使用i+1即可
         for(int i=0;i<strs[minIndex].length();i++){
-            str.append(strs[minIndex].charAt(i));
             j=0;
             while (j<= strs.length-1){
-                if(str.toString().equals(strs[j].substring(0,i+1))){
+                //改进这里，不用substring了，用charAt
+                if(strs[minIndex].charAt(i)==(strs[j].charAt(i))){
                     j++;
                 }else {
                     //这个break，是停止了之后的字符对于前缀的判断，因为已经有字符不满足该前缀了
@@ -35,14 +33,11 @@ public class Code14 {
             //这个break，是停止了对最短字符串的截取
             if(j!= strs.length){
                 break;
+            }else {
+                str.append(strs[minIndex].charAt(i));
             }
         }
-        //如果判断到了最后，j等于数组长度，说明最短的那个字符串，刚好就是最长的前缀；不然就不是，就相当于多判断了一次，所以需要往前截取一个字符
-        if(j==strs.length){
-            return str.toString();
-        }else {
-            return str.substring(0,str.length()-1);
-        }
+        return str.toString();
     }
 
     public static void main(String[] args) {
---
+++ test/leetcode/Code5.java
@@ -0,0 +1,67 @@
+package leetcode;
+//最长回文字符串
+public class Code5 {
+    public static String longestPalindrome(String s) {
+        char[] chars=s.toCharArray();
+        //滑动的左右窗口坐标
+        int left=0;
+        int right=1;
+        //最后确定的左右坐标
+        int start=0;
+        int end=1;
+        //滑动窗口的长度
+        int size=0;
+        //是否是回文字符串
+        boolean flag=false;
+        while(right<chars.length){
+            flag=isPalindrome(chars,left,right);
+            if(flag){
+                System.out.println("left:"+left);
+                System.out.println("right:"+right);
+                if(right-left>end-start){
+                    start=left;
+                    end=right;
+                }
+                if(left>0){
+                    left--;
+                }
+                if(right<chars.length){
+                    right++;
+                }
+            }else{
+                System.out.println("else--left:"+left);
+                System.out.println("else--right:"+right);
+                if((right-left)%2==0){
+                    left++;
+                }
+                if(right<chars.length){
+                    right++;
+                }
+            }
+        }
+        StringBuilder stringBuilder=new StringBuilder();
+        for (int i=start;i<=end;i++){
+            stringBuilder.append(chars[i]);
+        }
+        return stringBuilder.toString();
+    }
+
+    public static boolean isPalindrome(char[] chars,int left,int right){
+        while (left<right){
+            if(chars[left]==chars[right]){
+                left++;
+                right--;
+            }else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        char[] chars=new char[]{'b','a','b','a','d'};
+        String str="cbbd";
+        System.out.println(isPalindrome(chars,0,1));
+        System.out.println(longestPalindrome(str));
+    }
+}
