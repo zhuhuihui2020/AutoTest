7153697fa7aed992a562a1d409267576b7b0a341
zhuhuihui <zhuhh2019@163.com>
2022-08-09 15:10:20 +0800

    commit code485

---
+++ test/leetcode/Code485.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class Code485 {
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int maxCount=0;
+        int count=0;
+        for(int num:nums){
+            if(num==1){
+                count++;
+                if(count>maxCount){
+                    maxCount=count;
+                }
+            }else{
+                count=0;
+            }
+        }
+        return maxCount;
+    }
+
+    public static void main(String[] args) {
+        int[] nums=new int[]{1,1,0,1,1,1,1,0,1,0,0};
+        System.out.println(findMaxConsecutiveOnes(nums));
+    }
+}
