becac640706bcaf30bc639226da48ce3fd1cc675
zhuhuihui <zhuhh2019@163.com>
2022-08-15 17:30:04 +0800

    commit Code88_2

--- test/leetcode/Code88_2.java
+++ test/leetcode/Code88_2.java
@@ -18,17 +18,14 @@ public class Code88_2 {
             }else if(m<0&&n>=0){
                 nums1[total]=nums2[n];
                 n--;
-            }else{
-                nums1[total]=nums1[m];
-                m--;
             }
             total--;
         }
     }
 
     public static void main(String[] args) {
-        int[] nums1=new int[]{2,2,3,0,0,0};
-        int[] nums2=new int[]{2,5,6};
+        int[] nums1=new int[]{2,0};
+        int[] nums2=new int[]{1};
         int n=nums2.length;
         int m=nums1.length-n;
         merge(nums1,m,nums2,n);
