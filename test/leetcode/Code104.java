76934bd8422a042ace877a626c00ae02270b4398
zhuhuihui <zhuhh2019@163.com>
2022-09-23 17:48:29 +0800

    commit code104

---
+++ test/leetcode/Code104.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+//二叉树的最大深度
+public class Code104 {
+    public int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        int right=maxDepth(root.right);
+        int left=maxDepth(root.left);
+        return  Math.max(left,right)+1;
+        //Math.max更容易阅读和理解，其实Math.max用的也是三目运算符
+//        return (right>left?right:left)+1;
+    }
+
+}
